ee695d9572021f0dcdbdf926877aa933cf2e298a
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-06-17 13:04:35 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -35,32 +35,17 @@ fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   export TW_DEFAULT_LANGUAGE="en"
-	export OF_KEEP_FORCED_ENCRYPTION=0
   export OF_SCREEN_H=2160
   export OF_CLOCK_POS=1
 	export OF_USE_MAGISKBOOT=1
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=0
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=0
 	export OF_FORCE_PREBUILT_KERNEL=1
+ 	export FOX_USE_DYNAMIC_PARTITIONS=1
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
   export LC_ALL="C"
   export OF_VANILLA_BUILD=1
   export OF_AB_DEVICE=1
-
-	# use magisk 21.4 for the magisk addon
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-26.1.zip
-
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_REPLACE_BUSYBOX_PS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
 
         # use system (ROM) fingerprint where available
         export OF_USE_SYSTEM_FINGERPRINT=1
